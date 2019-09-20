227af01e4230e3228165315004fdd5a25cb96b51
Petr Šplíchal <psplicha@redhat.com>
2019-09-20 21:50:53 +0200

    Initial set of stories and tests

---
+++ tests/basic/test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Include Beaker environment
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+# Run all phases by default
+phase=${1:-all}
+examples="/usr/share/doc/fmf/examples/wget"
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm "fmf"
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+        rlRun "set -o pipefail"
+    rlPhaseEnd
+
+    [[ $phase =~ all|ls ]] && {
+    rlPhaseStartTest "Test listing available nodes"
+        rlRun "fmf ls --path $examples | tee output"
+        rlAssertGrep "/protocols/https" "output"
+        rlAssertNotGrep "priority: high" "output"
+    rlPhaseEnd; }
+
+    [[ $phase =~ all|show ]] && {
+    rlPhaseStartTest "Test showing node attributes"
+        rlRun "fmf show --path $examples | tee output"
+        rlAssertGrep "/protocols/https" "output"
+        rlAssertGrep "priority: high" "output"
+    rlPhaseEnd; }
+
+    [[ $phase =~ all|name ]] && {
+    rlPhaseStartTest "Test selecting nodes by name"
+        rlRun "fmf show --path $examples --name /recursion/deep | tee output"
+        rlAssertGrep "/recursion/deep" "output"
+        rlAssertGrep "depth: 1000" "output"
+    rlPhaseEnd; }
+
+    [[ $phase =~ all|filter ]] && {
+    rlPhaseStartTest "Test advanced filter"
+        rlRun "fmf ls --path $examples --filter priority:high | tee output"
+        rlAssertGrep "/requirements/protocols/https" "output"
+        rlAssertNotGrep "/requirements/protocols/progress" "output"
+    rlPhaseEnd; }
+
+    [[ $phase =~ all|condition ]] && {
+    rlPhaseStartTest "Test arbitrary condition"
+        rlRun "fmf ls --path $examples --condition 'depth < 100' | tee output"
+        rlAssertGrep "/recursion/fast" "output"
+        rlAssertNotGrep "/recursion/deep" "output"
+    rlPhaseEnd; }
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalEnd
---
+++ tests/docs/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Include Beaker environment
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+PACKAGE="fmf"
+EXAMPLES="/usr/share/doc/fmf/examples"
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlAssertRpm $PACKAGE
+        rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
+        rlRun "pushd $TmpDir"
+        rlRun "set -o pipefail"
+    rlPhaseEnd
+
+    rlPhaseStartTest "help"
+        rlRun "fmf --help | tee help" 0 "Run help"
+        rlAssertGrep "command line interface" "help"
+    rlPhaseEnd
+
+    rlPhaseStartTest "man"
+        rlRun "man fmf | tee man" 0 "Check man page"
+        rlAssertGrep "usage is straightforward" "man"
+    rlPhaseEnd
+
+    rlPhaseStartTest "examples"
+        rlRun "ls $EXAMPLES | tee examples" 0 "Check examples"
+        rlAssertGrep "wget" "examples"
+        rlRun "fmf ls --path $EXAMPLES/wget | tee wget"
+        rlAssertGrep "/protocols/https" "wget"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlRun "popd"
+        rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
+    rlPhaseEnd
+rlJournalEnd
