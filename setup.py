bc2b2183299d382350094e6a6f7ba2bdd0a6d777
Petr Šplíchal <psplicha@redhat.com>
2019-09-30 14:34:30 +0200

    Update the list of supported Python versions

--- setup.py
+++ setup.py
@@ -47,9 +47,9 @@ default_setup = dict(
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Utilities',
     ],
     keywords=['metadata', 'testing'],
